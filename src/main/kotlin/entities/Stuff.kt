e261fcf48fa022297124b49212fce46999d14ad7
GideonBrimleaf <chrisdryden1@gmail.com>
2020-10-08 10:35:13 +0100

    adds default value to table object though not sure it works

--- src/main/kotlin/entities/Stuff.kt
+++ src/main/kotlin/entities/Stuff.kt
@@ -2,6 +2,7 @@ package com.radiantchamber.walkarama.entities
 
 import dev.alpas.ozone.*
 import me.liuwj.ktorm.schema.int
+import me.liuwj.ktorm.schema.timestamp
 import java.time.Instant
 
 interface Stuff : OzoneEntity<Stuff> {
@@ -18,6 +19,6 @@ object Stuffs : OzoneTable<Stuff>("stuffs") {
     val id by bigIncrements()
     val name by string("name").size(150).nullable().bindTo { it.name }
     val numbero by int("numbero").nullable().bindTo { it.numbero }
-    val createdAt by createdAt()
-    val updatedAt by updatedAt()
+    val createdAt by timestamp("created_at").useCurrent().bindTo { it.createdAt }
+    val updatedAt by timestamp("updated_at").useCurrent().bindTo { it.updatedAt }
 }
