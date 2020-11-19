e630c227a73ab88afcaac30c3f6555e449467b43
GideonBrimleaf <chrisdryden1@gmail.com>
2020-11-19 16:46:23 +0000

    can successfully refer to map component

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -5,6 +5,7 @@ window.axios = axios
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
 Vue.component('welcome', require('./components/Welcome').default)
+Vue.component('google-map', require('./components/GoogleMap').default)
 
 new Vue({
     el: '#app'
---
+++ src/main/resources/js/components/GoogleMap.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Hello, This is the Map Component!</div>
+</template>
+
+<script>
+    export default {
+        name: "GoogleMap"
+    }
+</script>
+
+<style scoped>
+
+</style>
