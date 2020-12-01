c656602385debbbdd60e7d85a65eb449d02c7a40
GideonBrimleaf <chrisdryden1@gmail.com>
2020-12-01 17:03:18 +0000

    adds the todos

--- src/main/kotlin/start.kt
+++ src/main/kotlin/start.kt
@@ -4,6 +4,12 @@ import dev.alpas.Alpas
 import java.io.File
 
 fun main(args: Array<String>) {
+//    TODO User should just see one map - if they have a walk the pins are in place, if not it will prompt them to create a map
+//    TODO Update should have a warning window to extend/reduce the walk
+//    TODO Delete should reset the map without pins
+//    TODO Add authentication
+//    TODO Each user should only have 1 walk
+//    TODO Add invites
     val file = File(".env")
     if (!file.exists()) {
         file.createNewFile()
