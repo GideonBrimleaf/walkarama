fcfa43d45726b7e59a7cc19df6c3b8d591ba4a66
GideonBrimleaf <chrisdryden1@gmail.com>
2021-01-06 22:15:45 +0000

    tidying up todos

--- src/main/kotlin/start.kt
+++ src/main/kotlin/start.kt
@@ -4,10 +4,8 @@ import dev.alpas.Alpas
 import java.io.File
 
 fun main(args: Array<String>) {
-//    TODO Update should have a warning window to extend/reduce the walk
-//    TODO Update should be in the same window as walk view
 //    TODO Add authentication
-//    TODO Each user should only have 1 walk
+//    TODO Each user should only have 1 active walk but can see archived walks
 //    TODO Add invites
     val file = File(".env")
     if (!file.exists()) {
