dde312c7b00d7b7fffa08c00ef29365a51daabe6
GideonBrimleaf <chrisdryden1@gmail.com>
2021-01-29 11:14:01 +0000

    initial nav bar added

--- src/main/kotlin/start.kt
+++ src/main/kotlin/start.kt
@@ -5,8 +5,8 @@ import java.io.File
 
 fun main(args: Array<String>) {
 //    TODO Add navigation
-//    TODO Make this actually look decent
 //    TODO Add invites
+//    TODO Make this actually look decent
 //    TODO Show user progression along walk
 //    TODO User can see latest activity from walk (like projects)
     val file = File(".env")
