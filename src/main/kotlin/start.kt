6c6dc3e938dc8c399e370b6ee98488fff5bf8832
GideonBrimleaf <chrisdryden1@gmail.com>
2021-01-29 12:27:15 +0000

    adds more ideas to the todo list

--- src/main/kotlin/start.kt
+++ src/main/kotlin/start.kt
@@ -5,9 +5,10 @@ import java.io.File
 
 fun main(args: Array<String>) {
 //    TODO Add invites
+//    TODO User can see latest activity from walk (like projects)
 //    TODO Make this actually look decent
 //    TODO Show user progression along walk
-//    TODO User can see latest activity from walk (like projects)
+//    TODO Walk show page for archived walks - show map and activity list
     val file = File(".env")
     if (!file.exists()) {
         file.createNewFile()
