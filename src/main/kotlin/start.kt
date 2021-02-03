2b0045da1daf7ededb0fc1b235fc95d65ca0b532
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-03 15:07:48 +0000

    todo list for stride length

--- src/main/kotlin/start.kt
+++ src/main/kotlin/start.kt
@@ -6,6 +6,7 @@ import java.io.File
 fun main(args: Array<String>) {
 //    TODO Add invites
 //    TODO User can see latest activity from walk (like projects)
+//    TODO User can input estimated stride length for each step
 //    TODO Make this actually look decent
 //    TODO Show user progression along walk
 //    TODO Walk show page for archived walks - show map and activity list
