f592fbff9426135e28f46a8273f97988c40e41b2
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-05 15:13:42 +0000

    users can invite people to walk

--- src/main/kotlin/controllers/WalkMembershipController.kt
+++ src/main/kotlin/controllers/WalkMembershipController.kt
@@ -1,10 +1,29 @@
 package com.radiantchamber.walkarama.controllers
 
+import com.radiantchamber.walkarama.entities.Users
+import com.radiantchamber.walkarama.entities.WalkMemberships
 import dev.alpas.http.HttpCall
+import dev.alpas.orAbort
+import dev.alpas.ozone.create
 import dev.alpas.routing.Controller
+import me.liuwj.ktorm.dsl.eq
+import me.liuwj.ktorm.entity.findById
+import me.liuwj.ktorm.entity.findOne
 
 class WalkMembershipController : Controller() {
     fun add(call: HttpCall) {
-        call.reply("Hello, WalkMembershipController!")
+        val now = call.nowInCurrentTimezone().toInstant()
+        val email = call.stringParam("email").orAbort()
+        val invitee = Users.findOne {it.email eq email}.orAbort()
+
+       WalkMemberships.create {
+            it.walkId to call.longParam("id")
+            it.userId to invitee.id
+            it.createdAt to now
+            it.updatedAt to now
+        }
+
+        flash("success", "${invitee.name} <${invitee.email}> is now a member of this project")
+        call.redirect().back()
     }
 }
