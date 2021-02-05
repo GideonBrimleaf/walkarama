791165499f3da2288a5deab2ef5274a5c9c58095
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-05 17:06:54 +0000

    added todos for invite handling

--- src/main/kotlin/start.kt
+++ src/main/kotlin/start.kt
@@ -4,16 +4,17 @@ import dev.alpas.Alpas
 import java.io.File
 
 fun main(args: Array<String>) {
-//    TODO Invitation to a walk should deactivate any currently active walks and show shared walk
-//    TODO Users should be able to leave a walk they are a member of
+//    TODO User should not be able to deactivate a walk they are a member of
+//    TODO Users should be able to leave a walk they are a member of (set membership to inactive?)
 //    TODO Reactivating one of your walks should remove you from any walks you're a member of
 //    TODO Deactivating a current walk should archive it for both user and members (should already be done but check)
-//    TODO Invite should be mailed out and accepted on request
 //    TODO User can see latest activity from walk (like projects)
 //    TODO User can input estimated stride length for each step
 //    TODO Make this actually look decent
-//    TODO Show user progression along walk
+
 //    TODO Walk show page for archived walks - show map and activity list
+//    TODO Invite should be mailed out and accepted on request
+//    TODO Show user progression along walk
     val file = File(".env")
     if (!file.exists()) {
         file.createNewFile()
