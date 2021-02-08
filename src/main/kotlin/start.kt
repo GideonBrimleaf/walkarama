c2238aab7da6c9714621387b808ca9c7e64b80d3
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-08 16:17:19 +0000

    minor tweaks to task list

--- src/main/kotlin/start.kt
+++ src/main/kotlin/start.kt
@@ -8,8 +8,8 @@ fun main(args: Array<String>) {
 //    TODO User can input estimated stride length for each step
 //    TODO If invited - user should be redirected on all routes to a view to accept the invite
 //    TODO Make this actually look decent - progression bar for walk
-
 //    TODO Walk show page for archived walks - show map and activity list
+
 //    TODO Invite should be mailed out and accepted on request, this should show as pending for the inviter
 //    TODO Show user progression along walk in map
     val file = File(".env")
