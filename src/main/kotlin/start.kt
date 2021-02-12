52f821f09c2b62c7a9d7396cfb1d3caed5a546d7
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-12 09:25:09 +0000

    alpas logo redirects to current walk

--- src/main/kotlin/start.kt
+++ src/main/kotlin/start.kt
@@ -4,14 +4,13 @@ import dev.alpas.Alpas
 import java.io.File
 
 fun main(args: Array<String>) {
-//    TODO Walk show page for archived walks - show map and activity list
-//    TODO Alpas logo should redirect to current/new walk if logged in
 //    TODO Logging in should redirect to current/new walk
 //    TODO If invited - user should be redirected on all routes to a view to accept the invite
 //    TODO Need to add guard around walk invites
 //    TODO Make this actually look decent - progression bar for walk
 //    TODO Either try mailer services or bin email verification
 
+//    TODO Walk show page for archived walks - show map and activity list
 //    TODO Invite should be mailed out and accepted on request, this should show as pending for the inviter
 //    TODO Show user progression along walk in map
 //    TODO Unit Testing
