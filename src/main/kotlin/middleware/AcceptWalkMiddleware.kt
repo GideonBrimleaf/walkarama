3fd6324e2c6875b3878bb09ceff23bf4f90ce524
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-12 22:48:22 +0000

    initial logic for rerouting added - need to test

--- src/main/kotlin/middleware/AcceptWalkMiddleware.kt
+++ src/main/kotlin/middleware/AcceptWalkMiddleware.kt
@@ -1,13 +1,24 @@
 package com.radiantchamber.walkarama.middleware
 
 import com.radiantchamber.walkarama.entities.User
+import com.radiantchamber.walkarama.entities.WalkMemberships
 import dev.alpas.Handler
 import dev.alpas.Middleware
 import dev.alpas.http.HttpCall
+import me.liuwj.ktorm.dsl.eq
+import me.liuwj.ktorm.entity.findAll
+import me.liuwj.ktorm.entity.findList
 
 class AcceptWalkMiddleware : Middleware<HttpCall>() {
     override fun invoke(call: HttpCall, forward: Handler<HttpCall>) {
         val user = call.caller<User>()
-        forward(call)
+        val pendingMemberships =
+            WalkMemberships.findList { it.userId eq user.id }.filter { !it.accepted }
+
+        if (pendingMemberships.isNotEmpty()) {
+            call.redirect().toRouteNamed("walks.show_active")
+        } else {
+            forward(call)
+        }
     }
 }
