2f9d8a4df1e4ab13ffe0009c24ed84cccc15792a
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-17 11:37:26 +0000

    welcome page was redirecting to login page, disabling it

--- src/main/kotlin/controllers/WelcomeController.kt
+++ src/main/kotlin/controllers/WelcomeController.kt
@@ -4,7 +4,7 @@ import dev.alpas.http.HttpCall
 import dev.alpas.routing.Controller
 
 // https://alpas.dev/docs/controllers
-class WelcomeController : ApplicationController() {
+class WelcomeController : Controller() {
     fun index(call: HttpCall) {
         if (auth().user != null) call.redirect().toRouteNamed("walks.show_active")
         else call.render("welcome")
